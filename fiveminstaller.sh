f3bf22260e9bb008c8bd0441734e75f625c8ba3c
Squex0978 <123485001+Squex0978@users.noreply.github.com>
2023-06-26 14:03:22 +0200

    Update fiveminstaller.sh

--- fiveminstaller.sh
+++ fiveminstaller.sh
@@ -1,36 +1,33 @@
-#!/bin/bash
-
-echo 'FiveM linux Installer'
-
+echo 'FiveM Linux Installer'
 
 apt update && apt upgrade
 
-
 apt install xf tar
-
 
 mkdir -p /home/FiveM/server
 cd /home/FiveM/server
-
 
 echo 'Geben Sie den Link zu den FiveM-Artifakten ein:'
 read link
 wget $link
 
-
 echo 'Entpacken der FiveM-Dateien...'
 tar xf fx.tar.xz
-echo 'Artifacts Installiert'
+echo 'Artifacts installiert'
 
 rm -r fx.tar.xz
 
 echo 'Installieren von Screen...'
 apt install screen
 
+
+echo ' crontab wird installiert und eingerichtet'
+
+(crontab -l 2>/dev/null; echo "@reboot /bin/bash /home/FiveM/server/run.sh > /home/FiveM/server/cron.log 2>&1") | crontab -
+
+cd /home/FiveM/server && screen ./run.sh
+
 echo 'Erfolgreich installiert! Jetzt müssen Sie in den Ordner cd /home/FiveM/server wechseln und die Datei run.sh ausführen --> ./run.sh'
 
-cd /home/FiveM/server
+echo 'Außerdem wurde ein Crontab erfolgreich installiert. Bei einen Server neustart wird FIveM/Tx Admin automatisch Gestartet.'
 
-screen ./run.sh
-
-echo 'Fertig :)'
